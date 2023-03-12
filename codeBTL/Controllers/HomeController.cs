b082931cee6d256b8c03b8b129ea4d916168082b
KienDev <ngotrungkien2k2@gmail.com>
2023-03-12 12:47:24 +0700

    Add project files.

---
+++ codeBTL/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using codeBTL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace codeBTL.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SmartPhone()
+        {
+
+            return View();
+        }
+
+        public IActionResult PhoneAccessories()
+        {
+
+            return View();
+        }
+
+        public IActionResult News()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+        public IActionResult Feedback()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
