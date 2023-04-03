d60a20051b420d081f803e0064242f5755fc7929
KienDev <ngotrungkien2k2@gmail.com>
2023-04-04 00:17:44 +0700

    CRUD SmartPhone in admin page

--- codeBTL/Models/Chitietsp.cs
+++ codeBTL/Models/Chitietsp.cs
@@ -1,25 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace codeBTL.Models;
 
 public partial class Chitietsp
 {
+
+    [Required(ErrorMessage = "Mã chi tiết sản phẩm không được để trống.")]
+    [DisplayName("Mã chi tiết sản phẩm( CTSP-xx )")]
     public string MaChiTietSp { get; set; } = null!;
 
+
+    [Required(ErrorMessage = "Mã sản phẩm không được để trống.")]
+    [DisplayName("Mã sản phẩm( DT-xx, PK-xx )")]
     public string MaSp { get; set; } = null!;
 
+
+    [Required(ErrorMessage = "Đơn giá bán không được để trống.")]
+    [DisplayName("Đơn giá bán")]
     public decimal DonGiaBan { get; set; }
 
-    public decimal DonGiaNhap { get; set; }
+    [DisplayName("Đơn giá nhập")]
+    public decimal? DonGiaNhap { get; set; }
 
-    public string Ram { get; set; } = null!;
 
-    public string Rom { get; set; } = null!;
+    public string? Ram { get; set; } = null!;
 
-    public string Cpu { get; set; } = null!;
+    public string? Rom { get; set; } = null!;
 
-    public string Dlpin { get; set; } = null!;
+    public string? Cpu { get; set; } = null!;
 
-    public virtual Sanpham MaSpNavigation { get; set; } = null!;
+    [DisplayName("Dung lượng pin")]
+    public string? Dlpin { get; set; } = null!;
+
+    public virtual Sanpham? MaSpNavigation { get; set; } = null!;
 }
