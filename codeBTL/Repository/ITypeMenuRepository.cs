49aa7586629c894f5cb3e3b1190ddaea4a83ade3
Hoang Quan <89526802+hoangquan2311@users.noreply.github.com>
2023-04-14 15:25:05 +0700

    menu phu kien
    thêm Menu trang phụ kiện và sửa 1 số lỗi

---
+++ codeBTL/Repository/ITypeMenuRepository.cs
@@ -0,0 +1,12 @@
+﻿using codeBTL.Models;
+namespace codeBTL.Repository
+{
+	public interface ITypeMenuRepository
+	{
+		Loaisp Add(Loaisp loaiSp);
+        Loaisp Update(Loaisp loaiSp);
+        Loaisp Delete(string maLoai);
+        Loaisp GetLoaiSp(string maLoai);
+		IEnumerable<Loaisp> GetAllLoaiSp();
+	}
+}
